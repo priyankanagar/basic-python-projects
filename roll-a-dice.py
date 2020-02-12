1651c07d7f5f6c2ac0b53136d4dce69434dbf95d
priyanka nagar <6424470+priyankanagar@users.noreply.github.com>
2020-02-12 02:46:47 -0800

    simple monopoly dice simulator

--- roll-a-dice.py
+++ roll-a-dice.py
@@ -1,1 +1,18 @@
 
+
+import random
+
+
+while True:
+
+    d1 = random.randint(1,6)
+    d2 = random.randint(1,6)
+
+    print(d1)
+    print(d2)
+    print("The sum of the dice is ", (d1+d2))
+
+    if d1 == d2:
+        print("Doubles! Roll again!")
+
+    input("Press enter to roll again")
